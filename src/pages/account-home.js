d05990a0cbeeadb5de458349bfba986755bab93d
adewale-d-a <adewale-d-a.com>
2022-09-11 22:08:37 +0100

    dampling upload user story

--- src/pages/account-home.js
+++ src/pages/account-home.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import Cookies from "universal-cookie";
 import TopBar from "../components/top-bar";
 import "./pagesCss/account-home.css";
@@ -7,6 +9,45 @@ const AccountHome = () => {
   const cookies = new Cookies();
   const username = cookies.get("username");
 
+  // const LoadServerTwo = () => {
+  //   axios
+  //     .get("http://localhost:4000/", {
+  //       headers: { "Content-Type": "application/json" },
+  //       withCredentials: true,
+  //     })
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   LoadServerTwo();
+  // }, []);
+
+  const [description, setDescription] = useState("");
+  const [imageUpload, setImageUpload] = useState("");
+
+  const PushUpload = (e) => {
+    e.preventDefault();
+    let fileUpload = new FormData();
+    fileUpload.append("itemDescription", description);
+    fileUpload.append("imageUpload", imageUpload[0]);
+
+    axios
+      .put("http://localhost:4000/put/putItem", fileUpload, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <motion.div
@@ -23,6 +64,27 @@ const AccountHome = () => {
         <main>
           <h1>Welcome Home {username}</h1>
         </main>
+        <div>
+          <form
+            onSubmit={PushUpload}
+            method="post"
+            encType="multipart/form-data"
+          >
+            <input
+              type="file"
+              name="avatar"
+              // value={imageUpload}
+              onChange={(e) => setImageUpload(e.target.files)}
+            />
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              name="description"
+            />
+            <button type="submit">submit</button>
+          </form>
+        </div>
       </motion.div>
     </>
   );
